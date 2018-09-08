235a29c09077eb7af4ced4acd393836f7897ea8d
Mathewe <mathewebanda@gmail.com>
2018-09-08 15:49:02 -0400

    Skeleton of iOS app implemented in storyboard. Still need to make calendar and recipe views

---
+++ FoodOptimization/FoodOptimization/SecondView.swift
@@ -0,0 +1,41 @@
+//
+//  SecondView.swift
+//  FoodOptimization
+//
+//  Created by Mathewe on 9/8/18.
+//  Copyright © 2018 Jane Hsieh. All rights reserved.
+//
+
+import UIKit
+
+class SecondView: UIViewController {
+    
+    @IBOutlet weak var displayName: UILabel!
+    var myString = String()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        displayName.text = myString
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    
+}
--- FoodOptimization/FoodOptimization/ViewController.swift
+++ FoodOptimization/FoodOptimization/ViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet var inputName: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,47 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        return
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        var secondViewController = segue.destination as! SecondView
+        secondViewController.myString = "Hello " + inputName.text!
+    }
+    
+    @IBAction func savePrefs(_ sender: Any) {
+        if inputName.text! != ""
+        {
+            performSegue(withIdentifier: "mainSegue", sender: self)
+        }
+    }
+    
+    
+    @IBAction func vegetarian(_ sender: Any) {
+    }
+    
+    @IBAction func pescetarian(_ sender: Any) {
+    }
+    
+    @IBAction func vegan(_ sender: Any) {
+    }
+    
+    @IBAction func gluten(_ sender: Any) {
+    }
+    
+    @IBAction func dairy(_ sender: Any) {
+    }
+    
+    @IBAction func nuts(_ sender: Any) {
+    }
+    
+    
+    
 }
 
